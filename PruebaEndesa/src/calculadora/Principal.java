644e0c98ef67c6cb83aa3109030d50ff59641cb2
Daniel Martínez León <daniel1martinezleon@gmail.com>
2023-11-22 10:57:13 +0100

    Prueba editada

--- PruebaEndesa/src/calculadora/Principal.java
+++ PruebaEndesa/src/calculadora/Principal.java
@@ -1,9 +1,21 @@
 package calculadora;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		double potPunta = 0.09, potValle = 0.03, previoKilovatio = 0.12,
+				consumo1 = 0, consumo2 = 0, consumo3 = 0, potContratada = 0,
+				totConsumo = 0;
+		String aux;
+		char salir;
+		
+		do {
+			
+		}while(salir!="x");
 
 	}
 
